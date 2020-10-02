d48c0f11a55ff8f75396f51562ef10c9c09106eb
Jhaman <jhamank7@gmail.com>
2020-10-02 12:48:20 +0500

    - my first comment

---
+++ app/src/main/java/com/tapmad/jhamanci/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tapmad.jhamanci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
